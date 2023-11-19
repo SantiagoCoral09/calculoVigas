67f2ba7c33d3069f4fa03a2634da209fa48ff6e4
Santiago Coral <94382858+SantiagoCoral09@users.noreply.github.com>
2023-11-18 21:51:14 -0500

    Actualizar pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -56,3 +56,47 @@ function generarPDFVigas(){
     });
 }
 
+function generarPDFColumnas(){
+    const doc = new jsPDF('p','mm','letter');
+
+    let table_superior = document.getElementById('resultado');
+    let table_medio = document.getElementById('myChart');
+    let table_inferior = document.getElementById('resultado_cortante_cols');
+    let table_inferior2 = document.getElementById('zona_confinamiento_resultado');
+
+    // Agregar estilos CSS al encabezado del documento HTML
+    let style = '<style>h1 { font-size: 24px; font-weight: bold; text-align: center; font-family: Arial, Helvetica, sans-serif; color: black;}h2 { font-size: 20px; font-weight: bold; text-align: center;}table { text-align: center; } </style>';
+
+    // Crear un elemento HTML que contenga las tres tablas con títulos y subtítulos
+    let content = '<h1>REPORTE &nbsp;  RESULTADOS: <br> CÁLCULO &nbsp; DE &nbsp; COLUMNAS</h1><hr><br><br><h1>DISEÑO &nbsp; A &nbsp; FLEXIÓN</h1><br><div>';
+    content += table_superior.innerHTML;
+
+
+    // Agregar la imagen del gráfico al contenido HTML utilizando la etiqueta <img>
+    let imgData = table_medio.toDataURL('image/png');
+    if(imgData!=''){
+
+        content += '</div><br><br><div><h2>Gráfico</h2>';
+
+        content += '<img src="' + imgData + '" width="800px">';
+    
+    }
+    content += '</div><br><br><br><br><div><h1>CÁLCULOS  &nbsp;&nbsp; CORTANTE</h1>';
+    content += table_inferior.innerHTML;
+    content += '</div><br><br>';
+    content += table_inferior2.innerHTML;
+
+
+    // Agregar contenido HTML con estilos CSS al PDF utilizando el método doc.html()
+    doc.html(content+style,{
+        callback: function () {
+            // Abrir el archivo PDF en una nueva pestaña
+            doc.save('calculo_columnas.pdf');
+        },
+        margin:[20,5,20,25],
+        x:0,
+        y:0,
+        width:150,
+        windowWidth:675
+    });
+}
